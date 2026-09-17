63fc7670dec9a5be7f6c80ecfaed919e71700677
hamrakpetra <hamrak.petra@gmail.com>
2026-09-17 14:42:47 +0200

    test: add contact reveal and financing tests

--- cypress/pages/AdvertisementPage.ts
+++ cypress/pages/AdvertisementPage.ts
@@ -10,6 +10,38 @@ class AdvertisementPage {
     addToParking() {
         this.getParkingButton().click();
     }
+
+    getEmailRevealButton() {
+        return cy.get('[data-testid="seller-email"]').first();
+    }
+
+    getPhoneRevealButton() {
+        return cy.get('[data-testid="seller-phone-number-primary"]').first();
+    }
+
+    revealEmail() {
+        this.getEmailRevealButton().click();
+    }
+
+    revealPhone() {
+        this.getPhoneRevealButton().click();
+    }
+
+    getRevealedEmail() {
+        return cy.get(
+            '[data-controller="contact-reveal"] a[href^="mailto:"]'
+        ).first();
+    }
+
+    getRevealedPhone() {
+        return cy.get(
+            '[data-controller="contact-reveal"] a[href^="tel:"]'
+        ).first();
+    }
+
+    getFinancingLink() {
+        return cy.contains("a", "Finanszírozási kalkulátor");
+    }
 }
 
 export default new AdvertisementPage();
