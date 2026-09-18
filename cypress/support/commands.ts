31ae5f417dbe8c0e9bcabd53531fb92bf1871d1b
hamrakpetra <hamrak.petra@gmail.com>
2026-09-18 11:27:21 +0200

    test: handle didomi consent with cached session

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,25 +1,28 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            acceptDidomiConsent(): Chainable<void>;
+        }
+    }
+}
+
+Cypress.Commands.add("acceptDidomiConsent", () => {
+    cy.session(
+        "didomi-consent",
+        () => {
+            cy.visit("/");
+
+            cy.get("#didomi-notice-agree-button")
+                .should("be.visible")
+                .click();
+
+            cy.get('[data-testid="notice"]')
+                .should("not.exist");
+        },
+        {
+            cacheAcrossSpecs: true,
+        }
+    );
+});
+
+export {};
